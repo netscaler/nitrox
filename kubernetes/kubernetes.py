7627fa649aa6b5bc0b365947b5b3e12bb425b1ac
Chiradeep Vittal <chiradeep@apache.org>
2015-12-10 22:05:48 -0800

    support event stream

--- kubernetes/kubernetes.py
+++ kubernetes/kubernetes.py
@@ -145,7 +145,7 @@ class KubernetesInterface(object):
     def configure_ns_for_app(self, appname):
         backends = self.get_backends_for_app(appname)
         logger.info("Backends for %s are %s" % (appname, str(backends)))
-        #self.netskaler.configure_app(appname,  backends)
+        self.netskaler.configure_app(appname,  backends)
 
     def configure_ns_for_all_apps(self):
         appnames = map(lambda x:  x['name'], self.app_info['apps'])
